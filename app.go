6caf6eff04b01c369c67dd2aa24d7e23fad99bfd
alfman99 <amanresamntz@gmail.com>
2023-04-20 15:51:34 +0200

    basic funcionality working, ugly

--- app.go
+++ app.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"context"
-	"fmt"
 	"os/exec"
 )
 
@@ -22,10 +21,24 @@ func (a *App) startup(ctx context.Context) {
 	a.ctx = ctx
 }
 
+// Return codes from Creator.exe
+const (
+	SUCCESS                     int8 = 0
+	ERROR_ORIGINAL_PE_NOT_FOUND int8 = -1
+	ERROR_STUB_NOT_FOUND        int8 = -2
+	ERROR_EXE_TO_DLL            int8 = -3
+	ERROR_REGISTER_PROJECT      int8 = -4
+	UNEXPECTED_ERROR            int8 = -5
+)
+
 // Calls Creator.exe with the given arguments
-func (a *App) CallCreator(originalPe string, stubPath string, outputPath string, apiKey string) {
-	cmd := exec.Command("X:\\Carrera\\__TFG\\development\\__Protector\\CreatorDesktop\\Creator.exe", originalPe, stubPath, outputPath, apiKey)
+func (a *App) CallCreator(originalPe string, outputPath string, apiKey string) int8 {
+	cmd := exec.Command("X:\\Carrera\\__TFG\\development\\__Protector\\CreatorDesktop\\dependencies\\Creator.exe", originalPe, "X:\\Carrera\\__TFG\\development\\__Protector\\CreatorDesktop\\dependencies\\Stub.exe", outputPath, apiKey)
 	if err := cmd.Run(); err != nil {
-		fmt.Println(err)
+		if exitError, ok := err.(*exec.ExitError); ok {
+			return int8(exitError.ExitCode())
+		}
+		return UNEXPECTED_ERROR
 	}
+	return SUCCESS
 }
--- frontend/src/app.tsx
+++ frontend/src/app.tsx
@@ -1,18 +1,95 @@
-import './App.css'
-import logo from "./assets/images/logo-universal.png"
 import {CallCreator} from "../wailsjs/go/main/App";
 import {h} from 'preact';
+import {useEffect, useState} from "preact/hooks";
 
-export function App(props: any) {
-    function create() {
-      console.log("Hello World")
-      CallCreator("Hello World", "Hello World", "Hello World", "Hello");
+
+// Return codes from Creator.exe
+enum ReturnValues {
+  INIT                         = 1,
+	SUCCESS                      = 0,
+	ERROR_ORIGINAL_PE_NOT_FOUND  = -1,
+	ERROR_STUB_NOT_FOUND         = -2,
+	ERROR_EXE_TO_DLL             = -3,
+	ERROR_REGISTER_PROJECT       = -4,
+	UNEXPECTED_ERROR             = -5
+}
+
+
+export const App = () => {
+  const [responseCreator, setResponseCreator] = useState<ReturnValues>(1)
+  const [displayReturnMessage, setDisplayReturnMessage] = useState<string>("Welcome!")
+
+  const [originalPERoute, setOriginalPERoute] = useState<string>("")
+  const [outputPERoute, setOutputPERoute] = useState<string>("")
+  const [key, setKey] = useState<string>("")
+
+  const create = async () => {
+    const response = await CallCreator(
+      originalPERoute, 
+      outputPERoute, 
+      key
+    );
+    setResponseCreator(response as ReturnValues)
+  }
+
+  const handleReturnValue = (value: ReturnValues) => {
+    console.log(value)
+    switch (value) {
+      case ReturnValues.INIT: {
+        setDisplayReturnMessage("")
+        break;
+      }
+      case ReturnValues.SUCCESS: {
+        setDisplayReturnMessage("Success!")
+        break;
+      }
+      case ReturnValues.ERROR_ORIGINAL_PE_NOT_FOUND: {
+        setDisplayReturnMessage("Error: Original PE not found")
+        break;
+      }
+      case ReturnValues.ERROR_STUB_NOT_FOUND: {
+        setDisplayReturnMessage("Error: Stub not found")
+        break;
+      }
+      case ReturnValues.ERROR_EXE_TO_DLL: {
+        setDisplayReturnMessage("Error: Converting to DLL")
+        break;
+      }
+      case ReturnValues.ERROR_REGISTER_PROJECT: {
+        setDisplayReturnMessage("Error: Registering project")
+        break;
+      }
+      case ReturnValues.UNEXPECTED_ERROR: 
+      default: {
+        setDisplayReturnMessage("Error: Unexpected error")
+        break;
+      }
     }
+  }
 
-    return (
-        <>
-          <img src={logo} id="logo" alt="logo"/>
-          <button className="btn" onClick={create}>Greet</button>
-        </>
-    )
+  useEffect(() => {
+    handleReturnValue(responseCreator)
+  }, [responseCreator])
+
+  return (
+    <>
+      <div>
+        <label>Archivo original:</label>
+        <input type="file" id="original" name="original" onChange={e => setOriginalPERoute("")} value={originalPERoute} />
+      </div>
+
+      <div>
+        <label>Ruta output:</label>
+        <input type="file" id="original" name="original" onChange={e => setOutputPERoute("")} value={outputPERoute} />
+      </div>
+
+      <div>
+        <label>API KEY:</label>
+        <input type="file" id="original" name="original" onChange={e => setKey("")} value={key} />
+      </div>
+
+      <button className="btn" onClick={create}>Protect!</button>
+      <text>{displayReturnMessage}</text>
+    </>
+  )
 }
